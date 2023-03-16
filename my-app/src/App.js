6d55b17a89b0e761bf4f0d8262cd0f93d4f5ad9f
DiegoMC28 <diego010128@gmail.com>
2023-03-16 11:40:45 +0100

    *

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
   const expenses = [
@@ -27,6 +28,7 @@ function App() {
 
   return (
     <div>
+      <NewExpense></NewExpense>
       <Expenses items={expenses}></Expenses>
     </div>
   );
