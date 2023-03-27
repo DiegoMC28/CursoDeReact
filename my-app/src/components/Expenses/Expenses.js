8f9a99ee8827fbcdee3ce96ee796b8c7b049b610
DiegoMC28 <diego010128@gmail.com>
2023-03-27 19:53:44 +0200

    *

--- my-app/src/components/Expenses/Expenses.js
+++ my-app/src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import Card from "../UI/Card";
 import React, { useState } from "react";
 import ExpensesList from "./ExpenseList";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -22,6 +23,7 @@ const Expenses = (props) => {
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></ExpensesFilter>
+            <ExpensesChart expenses = {filteredExpenses}></ExpensesChart>
             <ExpensesList items = {filteredExpenses}></ExpensesList>
         </Card>
     );
